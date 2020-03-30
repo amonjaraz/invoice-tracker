676943004d4ff102ab8a0f15f869a8408e1b4a22
alexmonjaraz-at-890488113368 <monjaraz.alexandro@gmail.com>
2020-03-30 15:48:28 -0500

    added nav bar using thymeleaf fragments and page layout

--- src/main/java/com/alexmonjaraz/invoicetracker/InvoiceTrackerApplication.java
+++ src/main/java/com/alexmonjaraz/invoicetracker/InvoiceTrackerApplication.java
@@ -2,10 +2,18 @@ package com.alexmonjaraz.invoicetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 @SpringBootApplication
 public class InvoiceTrackerApplication {
 
+	@Bean
+	public LayoutDialect layoutDialect() {
+	    return new LayoutDialect();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(InvoiceTrackerApplication.class, args);
 	}
